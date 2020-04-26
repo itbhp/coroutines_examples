3dccfd3cd1df6cb301097e88b165da5aece5c740
Paolo Sciarra <paolo.sciarra@lastminute.com>
2020-04-26 15:45:00 +0200

    used runBlocking on main (introducing child coroutines)

---
+++ src/main/kotlin/coroutines/ChildCoroutine.kt
@@ -0,0 +1,15 @@
+package coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking { // start main coroutine
+    GlobalScope.launch { // launch a new coroutine in background and continue (child of runBlocking)
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,") // main coroutine continues here immediately
+    delay(2000L)      // delaying for 2 seconds to keep JVM alive
+}
