083ce777739e9dbd08adf57e2341d265bd0edb8a
Paolo Sciarra <paolo.sciarra@lastminute.com>
2020-04-26 16:37:03 +0200

    coroutines scope builder for parallel job execution

--- src/main/kotlin/coroutines/ThreadExample.kt
+++ src/main/kotlin/coroutines/ThreadExample.kt
@@ -5,8 +5,8 @@ import kotlin.concurrent.thread
 fun main() {
     thread {
         Thread.sleep(1000L)
-        println("World!") // print after delay
+        println("World!")
     }
-    println("Hello,") // main thread continues while coroutine is delayed
-    Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+    println("Hello,")
+    Thread.sleep(2000L)
 }
