21cee31446733a8bd42393c5d9f545861e2e6e7d
Paolo Sciarra <paolo.sciarra@lastminute.com>
2020-04-26 19:29:15 +0200

    job cancel example

---
+++ src/main/kotlin/coroutines/JobCancellationExample.kt
@@ -0,0 +1,18 @@
+package coroutines
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = launch {
+        repeat(1000) { i ->
+            println("job: I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancel() // cancels the job
+    println("main: Now I can quit.")
+}
